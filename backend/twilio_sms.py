0b29aa675e73d8ed22e5425e1487b08668af59fa
JEphron <jephron63@gmail.com>
2015-08-09 05:42:54 -0400

    Remove phone number

--- backend/twilio_sms.py
+++ backend/twilio_sms.py
@@ -1,5 +1,5 @@
 from twilio.rest import TwilioRestClient
-
+from constants import *
 
 def send_text(reward, task):
     # Your Account Sid and Auth Token from twilio.com/user/account
@@ -8,5 +8,5 @@ def send_text(reward, task):
     client = TwilioRestClient(account_sid, auth_token)
     url = ""
     message = client.messages.create(body="Congratz! You earned %s for completing %s. Check out your money at %s" % (reward, task, url),
-                                     to="+1 646-667-7202", from_="+1 646-542-0738")
+                                     to=TARGET_PHONE_NUMBER, from_="+1 646-542-0738")
     print message.sid
